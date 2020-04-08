976a08faea18ea3851948b159051993b616edcc9
kingsleyitguy <35885316+kingsleyitguy@users.noreply.github.com>
2020-04-07 18:31:11 -0600

    Add files via upload

---
+++ flickr.js
@@ -0,0 +1,68 @@
+(function (document, window) {
+  "use strict";
+
+  var apiKey = "6899306c03d4a8e31e1b1e653a611f97";
+  var apiURL = "https://api.flickr.com/services/rest/";
+
+  function searchText(parameters) {
+    var requestParameters = Utility.extend(parameters, {
+      method: "flickr.photos.search",
+      api_key: apiKey,
+      format: "json",
+    });
+
+    var script = document.createElement("script");
+    script.src = Utility.buildUrl(apiURL, requestParameters);
+    document.head.appendChild(script);
+    document.head.removeChild(script);
+  }
+
+  function buildThumbnailUrl(photo) {
+    return (
+      "https://farm" +
+      photo.farm +
+      ".staticflickr.com/" +
+      photo.server +
+      "/" +
+      photo.id +
+      "_" +
+      photo.secret +
+      "_q.jpg"
+    );
+  }
+
+  function buildPhotoUrl(photo) {
+    return (
+      "https://farm" +
+      photo.farm +
+      ".staticflickr.com/" +
+      photo.server +
+      "/" +
+      photo.id +
+      "_" +
+      photo.secret +
+      ".jpg"
+    );
+  }
+
+  function buildPhotoLargeUrl(photo) {
+    return (
+      "https://farm" +
+      photo.farm +
+      ".staticflickr.com/" +
+      photo.server +
+      "/" +
+      photo.id +
+      "_" +
+      photo.secret +
+      "_b.jpg"
+    );
+  }
+
+  window.Flickr = Utility.extend(window.Flickr || {}, {
+    buildThumbnailUrl: buildThumbnailUrl,
+    buildPhotoUrl: buildPhotoUrl,
+    buildPhotoLargeUrl: buildPhotoLargeUrl,
+    searchText: searchText,
+  });
+})(document, window);
